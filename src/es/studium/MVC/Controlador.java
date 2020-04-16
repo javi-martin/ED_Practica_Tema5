ef9dcba2899fe2a0f66b0f44b57a8faeebaf7191
Javier Martin <jamaga@outlook.es>
2020-04-16 14:30:02 +0200

    1º Versión

---
+++ src/es/studium/MVC/Controlador.java
@@ -0,0 +1,279 @@
+package es.studium.MVC;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.sql.Connection;
+
+
+public class Controlador implements WindowListener, ActionListener 
+{
+	Modelo modelo = null;	
+	Vista vista = null;
+	
+	public Controlador(Modelo modelo, Vista vista) 
+	{	
+		this.modelo = modelo;		
+		this.vista = vista;
+		
+		// Añadimos el listener a cada opción de submenú del menú Artículos
+		
+		vista.nuevo.addActionListener(this);		
+		vista.eliminar.addActionListener(this);		
+		vista.consultar.addActionListener(this);
+		vista.modificar.addActionListener(this);
+		
+		vista.btnNuevoEmpleado.addActionListener(this);
+		vista.btnCancelarNuevoEmpleado.addActionListener(this);
+		
+		vista.btnEliminarEmpleado.addActionListener(this);
+		vista.btnCancelarEliminarEmpleado.addActionListener(this);
+		
+		vista.btnConsultarEmpleado.addActionListener(this);
+		vista.btnSalirConsultarEmpleado.addActionListener(this);
+		
+		vista.btnSelecionarEmpleado.addActionListener(this);
+		vista.btnModificarEmpleado.addActionListener(this);
+		vista.btnCancelarModificarEmpleado.addActionListener(this);
+		
+		vista.btnSalir.addActionListener(this);
+		
+		// Añadimos el listener a las ventanas
+		
+		vista.addWindowListener(this);
+		
+		// Para poder cerrar el Diálogo, añadimos el listener a los cuadro de diálogo
+		
+		vista.dialogoNuevo.addWindowListener(this);
+		vista.dialogoEliminar.addWindowListener(this);
+		vista.dialogoConsultar.addWindowListener(this);
+		vista.dialogoModificar.addWindowListener(this);
+		vista.dialogoInformar.addWindowListener(this);
+	
+	}
+	
+	public void actionPerformed(ActionEvent ae) 
+	{
+		
+		Object a;
+		
+		a = ae.getSource();
+		
+		if (a.equals(vista.nuevo))
+		{
+			//añade la etiqueta la cuadro de diálogo y la hace visible
+			
+			vista.dialogoNuevo.add(vista.lblNombreEmpleado);
+			vista.dialogoNuevo.add(vista.txtNombreEmpleado);
+			vista.dialogoNuevo.add(vista.btnNuevoEmpleado);
+			vista.dialogoNuevo.add(vista.btnCancelarNuevoEmpleado);
+			vista.dialogoNuevo.setVisible(true);		
+		}
+		else if (a.equals(vista.btnCancelarNuevoEmpleado)) 
+		{
+			vista.dialogoNuevo.setVisible(false);
+		}
+		else if (a.equals(vista.btnNuevoEmpleado)) 
+		{
+			// Conectar BD
+			Connection con = modelo.conectar("empresa","root","Studium2019;");
+			
+			// Hacer el INSERT
+			int respuesta = modelo.insertar(con, "empleados", vista.txtNombreEmpleado.getText());
+			
+			// Mostramos resultado
+			if(respuesta == 0)
+			{
+				System.out.println("ALTA de empleado correcta");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("ALTA de empleado correcta"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);				
+			}
+			else
+			{
+				System.out.println("Error en ALTA de empleado");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("Error en ALTA de empleado"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);	
+			}
+			
+			vista.txtNombreEmpleado.selectAll();
+			vista.txtNombreEmpleado.setText("");
+			
+			// Desconectar de la base
+			modelo.desconectar(con);
+		}
+		else if (a.equals(vista.eliminar)) 
+		{
+			vista.dialogoEliminar.add(vista.lblEliminarEmpleado);
+			vista.dialogoEliminar.add(vista.choEmpleado);
+			modelo.rellenarChoice(vista.choEmpleado);
+			vista.dialogoEliminar.add(vista.btnEliminarEmpleado);
+			vista.dialogoEliminar.add(vista.btnCancelarEliminarEmpleado);
+			vista.dialogoEliminar.setVisible(true);
+		}
+		else if (a.equals(vista.btnCancelarEliminarEmpleado)) 
+		{
+			vista.dialogoEliminar.setVisible(false);
+		}
+		else if (a.equals(vista.btnEliminarEmpleado)) 
+		{
+			// Conectar a BD
+			Connection con = modelo.conectar("empresa","root","Studium2019;"); 
+			
+			// Borrar
+			String[] Empleado=vista.choEmpleado.getSelectedItem().split("-");
+			int respuesta = modelo.borrar(con, "empleados","idEmpleado",Integer.parseInt(Empleado[0]));
+			
+			// Mostramos resultado
+			if(respuesta == 0)
+			{
+				System.out.println("BAJA de empleado correcta");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("BAJA de empleado correcta"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);				
+			}
+			else
+			{
+				System.out.println("Error en BAJA de empleado");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("Error en BAJA de empleado"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);	
+			}
+			vista.choEmpleado.removeAll();
+			modelo.rellenarChoice(vista.choEmpleado);
+			// Desconectar de la base
+			modelo.desconectar(con);
+		}
+		else if (a.equals(vista.modificar)) 
+		{
+			vista.dialogoModificar.add(vista.lblModificarEmpleado);
+			vista.dialogoModificar.add(vista.choEmpleado);
+			modelo.rellenarChoice(vista.choEmpleado);
+			vista.dialogoModificar.add(vista.btnSelecionarEmpleado);
+			vista.dialogoModificar.add(vista.lblNombreEmpleado);
+			vista.dialogoModificar.add(vista.txtNombreEmpleado);
+			vista.dialogoModificar.add(vista.btnModificarEmpleado);
+			vista.dialogoModificar.add(vista.btnCancelarModificarEmpleado);
+			vista.dialogoModificar.setVisible(true);
+		}
+		else if (a.equals(vista.btnCancelarModificarEmpleado)) 
+		{
+			vista.dialogoModificar.setVisible(false);
+		}
+		else if (a.equals(vista.btnSelecionarEmpleado)) 
+		{			
+			String[] Empleado=vista.choEmpleado.getSelectedItem().split("-");
+			vista.txtNombreEmpleado.setText(Empleado[1]);
+		}
+		else if (a.equals(vista.btnModificarEmpleado)) 
+		{
+			// Conectar a BD
+			Connection con = modelo.conectar("empresa","root","Studium2019;"); 
+			
+			// Borrar
+			String[] Empleado=vista.choEmpleado.getSelectedItem().split("-");
+			
+			int respuesta = modelo.modificar(con,vista.txtNombreEmpleado.getText(),Integer.parseInt(Empleado[0]));
+			
+			// Mostramos resultado
+			if(respuesta == 0)
+			{
+				System.out.println("MODIFICACIÓN de empleado correcta");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("MODIFICACIÓN empleado correcta"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);				
+			}
+			else
+			{
+				System.out.println("Error en MODIFICACIÓN de empleado");
+				vista.dialogoInformar.add(vista.lblEtiqueta1);
+				vista.lblEtiqueta1.setText(modelo.rellenarLabel("Error MODIFICACIÓN empleado"));
+				vista.dialogoInformar.add(vista.btnSalir);
+				vista.dialogoInformar.setVisible(true);	
+			}
+			vista.txtNombreEmpleado.selectAll();
+			vista.txtNombreEmpleado.setText("");
+			vista.choEmpleado.removeAll();
+			modelo.rellenarChoice(vista.choEmpleado);
+			
+			// Desconectar de la base
+			modelo.desconectar(con);			
+		}		
+		else if(a.equals(vista.consultar)) 
+		{
+			vista.dialogoConsultar.add(vista.panel1);
+			vista.panel1.add(vista.lblIdEmpleado);
+			vista.panel1.add(vista.txtIdEmpleado);
+			vista.dialogoConsultar.add(vista.panel2);
+			vista.panel2.add(vista.btnConsultarEmpleado);
+			vista.dialogoConsultar.add(vista.panel3);
+			vista.panel3.add(vista.txtIdEmpleado2);
+			vista.txtIdEmpleado2.setEnabled(false);
+			vista.panel3.add(vista.txtNombreEmpleado2);
+			vista.txtNombreEmpleado2.setEnabled(false);
+			vista.dialogoConsultar.add(vista.btnSalirConsultarEmpleado);
+			vista.dialogoConsultar.setVisible(true);
+		}
+		else if (a.equals(vista.btnSalirConsultarEmpleado)) 
+		{
+			vista.dialogoConsultar.setVisible(false);
+		}
+		else if (a.equals(vista.btnConsultarEmpleado)) 
+		{
+			// Conectar a BD
+			Connection con = modelo.conectar("empresa","root","Studium2019;"); 
+			
+			modelo.mostrarDatos(con, Integer.parseInt(vista.txtIdEmpleado.getText()), vista.txtIdEmpleado2, vista.txtNombreEmpleado2);
+			
+			// Desconectar de la base
+			modelo.desconectar(con);
+		}
+		else 
+		{
+			vista.dialogoInformar.setVisible(false);
+		}
+		
+	}
+	
+	public void windowActivated(WindowEvent arg0) {}	
+	public void windowClosed(WindowEvent arg0) {}		
+	public void windowClosing(WindowEvent we) {
+	
+		if (vista.dialogoNuevo.isActive())
+		{		
+			vista.dialogoNuevo.setVisible(false);		
+		} 
+		else if (vista.dialogoEliminar.isActive())
+		{		
+			vista.dialogoEliminar.setVisible(false);		
+		}
+		else if (vista.dialogoConsultar.isActive())
+		{		
+			vista.dialogoConsultar.setVisible(false);		
+		} 
+		else if (vista.dialogoModificar.isActive())
+		{		
+			vista.dialogoModificar.setVisible(false);		
+		}
+		else if (vista.dialogoInformar.isActive())
+		{		
+			vista.dialogoInformar.setVisible(false);		
+		} 
+		else
+		{		
+			System.exit(0);		
+		}
+	
+	}	
+	public void windowDeactivated(WindowEvent arg0) {}	
+	public void windowDeiconified(WindowEvent arg0) {}		
+	public void windowIconified(WindowEvent arg0) {	}
+	public void windowOpened(WindowEvent arg0) {}
+}
---
+++ src/es/studium/MVC/Vista.java
@@ -0,0 +1,135 @@
+package es.studium.MVC;
+
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.Dialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class Vista extends Frame
+{
+	private static final long serialVersionUID = 1L;
+
+	// Declarar la Barra de Menú
+
+	MenuBar barraMenu = new MenuBar();
+
+	// Declarar las opciones de la Barra de Menú
+
+	Menu empleados = new Menu("Empleados");
+	
+	// Declarar las opciones del submenú de Empleado
+
+	MenuItem nuevo = new MenuItem("Nuevo Empleado");
+	MenuItem eliminar = new MenuItem("Eliminar Empleado");
+	MenuItem consultar = new MenuItem("Consultar Empleado");
+	MenuItem modificar = new MenuItem("Modificar Empleado");
+	
+	// Creamos el cuadro de diálogo Nuevo Empleado
+
+	Dialog dialogoNuevo = new Dialog(this, "Nuevo Empleado", true);	
+	Label lblNombreEmpleado = new Label("Nombre:");
+	TextField txtNombreEmpleado = new TextField(20);
+	Button btnNuevoEmpleado = new Button("Acepta");
+	Button btnCancelarNuevoEmpleado = new Button("Cancelar");
+	
+	// Creamos el cuadro de diálogo Eliminar Empleado
+
+	Dialog dialogoEliminar = new Dialog(this, "Eliminar Empleado", true);
+	Label lblEliminarEmpleado = new Label("Empleado a borrar:");
+	Choice choEmpleado = new Choice();
+	Button btnEliminarEmpleado = new Button("Borrar");
+	Button btnCancelarEliminarEmpleado = new Button("Cancelar");
+	
+	// Creamos el cuadro de diálogo Consultar Empleado
+
+	Panel panel1 = new Panel();
+	Panel panel2 = new Panel();
+	Panel panel3 = new Panel();
+	Dialog dialogoConsultar = new Dialog(this, "Consultar Empleado", true);
+	Label lblIdEmpleado = new Label("Introducir número de empleado:");
+	TextField txtIdEmpleado = new TextField(5);
+	TextField txtIdEmpleado2 = new TextField(5);
+	TextField txtNombreEmpleado2 = new TextField(20);
+	Button btnConsultarEmpleado = new Button("Consultar");
+	Button btnSalirConsultarEmpleado = new Button("Salir");
+	
+	// Creamos el cuadro de diálogo Modificar Empleado
+
+	Dialog dialogoModificar = new Dialog(this, "Modificar Empleado", true);
+	Label lblModificarEmpleado = new Label("Empleado a modificar:");
+	Button btnSelecionarEmpleado = new Button("Selecionar");
+	Button btnModificarEmpleado = new Button("Modificar");
+	Button btnCancelarModificarEmpleado = new Button("Cancelar");
+	
+	// Creamos el cuadro de diálogo Informar Resultado
+
+	Dialog dialogoInformar = new Dialog(this,"Información", true);
+	Label lblEtiqueta1 = new Label();	
+	Button btnSalir = new Button("Salir");		
+		
+	public Vista() 
+	{
+		setLayout(new FlowLayout());	
+		setTitle("Menú");
+	
+		//Establecemos la barra de menú como tal
+	
+		setMenuBar(barraMenu);		
+	
+		//Añadimos al menú Artículo las opciones de submenú, con separador entre ellas
+	
+		empleados.add(nuevo);	
+		empleados.addSeparator();	
+		empleados.add(eliminar);	
+		empleados.addSeparator();	
+		empleados.add(consultar);
+		empleados.addSeparator();	
+		empleados.add(modificar);
+		
+		//Añadimos las opciones de menú a la barra de menú
+		
+		barraMenu.add(empleados);
+		
+		setSize(350,200);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		//Establecemos el layout del cuadro de diálogo Nuevo Empleado y su tamaño
+		
+		dialogoNuevo.setLayout(new FlowLayout());	
+		dialogoNuevo.setSize(300,150);
+		dialogoNuevo.setLocationRelativeTo(null);
+		
+		//Establecemos el layout del cuadro de diálogo Eliminar Empleado y su tamaño
+		
+		dialogoEliminar.setLayout(new FlowLayout());	
+		dialogoEliminar.setSize(220,150);
+		dialogoEliminar.setLocationRelativeTo(null);
+		
+		//Establecemos el layout del cuadro de diálogo Consultar Empleado y su tamaño
+		
+		dialogoConsultar.setLayout(new FlowLayout());	
+		dialogoConsultar.setSize(300,200);
+		dialogoConsultar.setLocationRelativeTo(null);
+		
+		//Establecemos el layout del cuadro de diálogo Modificar Empleado y su tamaño
+		
+		dialogoModificar.setLayout(new FlowLayout());	
+		dialogoModificar.setSize(300,200);
+		dialogoModificar.setLocationRelativeTo(null);
+		
+		//Establecemos el layout del cuadro de diálogo Modificar Empleado y su tamaño
+		
+		dialogoInformar.setLayout(new FlowLayout());	
+		dialogoInformar.setSize(230,150);
+		dialogoInformar.setLocationRelativeTo(null);
+	}	
+
+}
